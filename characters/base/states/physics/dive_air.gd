9677f7578d197b86890ffef5673ab8f082bafa06
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2025-11-14 09:15:05 +0100

    make air dive physics more accurate

--- characters/base/states/physics/dive_air.gd
+++ characters/base/states/physics/dive_air.gd
@@ -6,3 +6,13 @@ extends AirPhysics
 ## in the character's current pool of states
 func _startup_check() -> bool:
 	return false
+
+
+## runs every frame while active
+func _update() -> void:
+	## Gravity
+	var total_gravity: float = character.get_gravity_sum()
+	character.velocity.y += total_gravity
+	
+	apply_air_drag(total_gravity)
+	do_air_movement(character.input["left"][0], character.input["right"][0])
