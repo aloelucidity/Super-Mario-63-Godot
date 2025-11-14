a83f1ac2b1e09319dd209f5b271f79b86ec6110b
aloelucidity <106764798+aloelucidity@users.noreply.github.com>
2025-11-14 09:10:51 +0100

    fix crash

--- characters/base/states/physics/movement.gd
+++ characters/base/states/physics/movement.gd
@@ -30,7 +30,7 @@ func do_friction() -> void:
 
 @export_group("Air Movement")
 @export var air_move_speed: float
-@export var max_air_move_speed: float
+@export var max_air_move_speed: float = 1
 @export var speed_divider: float = 1 ## used by some states to slow movement instead of changing move speed vars
 @export var max_speed_divider: float = 3 ## same as above
 
